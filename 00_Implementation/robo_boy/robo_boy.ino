b612a58d599139cb5e159ba9d559e9d0a2978b2c
Popa <uib00020@contiwan.com>
2020-04-06 20:23:19 +0300

    update

--- 00_Implementation/robo_boy/robo_boy.ino
+++ 00_Implementation/robo_boy/robo_boy.ino
@@ -24,6 +24,7 @@ int i;
 int uart[9];  //save data measured by LiDAR
 const int HEADER=0x59;  //frame header of data package
 
+int pwm_speed = 100;
 void setup() {
   // Set all the motor control pins to outputs
   pinMode(enA, OUTPUT);
@@ -44,27 +45,77 @@ void setup() {
 }
 
 void loop() { 
-   //Serial.println("Test"); //output signal strength value
-  if (Serial1.available()) {  //check if serial port has data input
-    if(Serial1.read() == HEADER) {  //assess data package frame header 0x59
-      uart[0]=HEADER;
-      if (Serial1.read() == HEADER) { //assess data package frame header 0x59
-        uart[1] = HEADER;
-        for (i = 2; i < 9; i++) { //save data in array
-          uart[i] = Serial1.read();
-        }
-        check = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
-        if (uart[8] == (check & 0xff)){ //verify the received data as per protocol
-          dist = uart[2] + uart[3] * 256;     //calculate distance value
-          strength = uart[4] + uart[5] * 256; //calculate signal strength value
-          Serial.print("dist = ");
-          Serial.print(dist); //output measure distance value of LiDAR
-          Serial.print('\t');
-          Serial.print("strength = ");
-          Serial.print(strength); //output signal strength value
-          Serial.print('\n');
-        }
-      }
-    }
-  }
+
+  analogWrite(enA, pwm_speed);
+  analogWrite(enB, pwm_speed);
+
+//  if (Serial1.available()) {  //check if serial port has data input
+//    if(Serial1.read() == HEADER) {  //assess data package frame header 0x59
+//      uart[0]=HEADER;
+//      if (Serial1.read() == HEADER) { //assess data package frame header 0x59
+//        uart[1] = HEADER;
+//        for (i = 2; i < 9; i++) { //save data in array
+//          uart[i] = Serial1.read();
+//        }
+//        check = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
+//        if (uart[8] == (check & 0xff)){ //verify the received data as per protocol
+//          dist = uart[2] + uart[3] * 256;     //calculate distance value
+//          strength = uart[4] + uart[5] * 256; //calculate signal strength value
+//          Serial.print("dist = ");
+//          Serial.print(dist); //output measure distance value of LiDAR
+//          Serial.print('\t');
+//          Serial.print("strength = ");
+//          Serial.print(strength); //output signal strength value
+//          Serial.print('\n');
+//        }
+//      }
+//    }
+//  }
+  delay(2000);
+  forward();
+  delay(4000);
+  leftward();
+  delay(1000);
+  forward();
+  delay(4000);
+  backward();
+  delay(4000);
+  rightward();
+  delay(1000);
+  Stop();
+  if(pwm_speed<=255)
+    pwm_speed+=25;
+  else
+    pwm_speed=100;
+}
+
+void backward() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+void forward() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+void rightward() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+void leftward() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+void Stop() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
