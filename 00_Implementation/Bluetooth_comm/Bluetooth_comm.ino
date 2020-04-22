3a552091c3a2d3aedbcf8b8fc90276f598b76728
Alin Laptop <alin_laptop@gmail.com>
2020-04-22 07:57:07 +0300

    update

--- 00_Implementation/Bluetooth_comm/Bluetooth_comm.ino
+++ 00_Implementation/Bluetooth_comm/Bluetooth_comm.ino
@@ -1,31 +1,40 @@
-int a1 = 0;
-int a2 = 0;
+#include <SoftwareSerial.h>  //header file of software serial port
+
+#define Tx 8
+#define Rx 9
+SoftwareSerial Serial1(8, 9); //define software serial port name as Serial1 and define pin2 as RX and pin3 as TX
+
+
+//int a1 = 0;
+//int a2 = 0;
 int val = 0;
 
 void setup()
 {
   Serial.begin(9600);
+  Serial1.begin(19200);
+
+  Serial.println("Start");
 }
 
 void loop()
 {
-  a1 = analogRead(0);
-  a2 = analogRead(1);
-  if (Serial.available() > 0) {
-
+  //  Tx = analogRead(8);
+  //  Rx = analogRead(9);
+  if (Serial1.available() > 0) {
     /* whatever is available from the serial is read here    */
-    val = Serial.read();
+    val = Serial1.read();
+    Serial.println(val);
 
+    if (val == 10)
     {
-
-      if (val == 50)
-      {
-        Serial.println(a1);
-      }
-      else if (val == 20)
-      {
-        Serial.println(a2);
-      }
+      Serial1.print(1000);
+      val = 0; 
+      Serial.println("Message has been sent");
     }
+//    else if (val == 20)
+//    {
+//      Serial.println(val);
+//    }
   }
 }
--- 00_Implementation/Matlab_code/Bluetooth_comm.m
+++ 00_Implementation/Matlab_code/Bluetooth_comm.m
@@ -2,12 +2,14 @@
 
 b = Bluetooth('HC-05',1);
 fopen(b);
-  
-    fwrite(b,50)
-    q=fscanf(b,'%d')
-    fwrite(b,20)
-    z=fscanf(b,'%d')
-
+disp('Bluetooth opened succesfully')
+%for i=1:10
+    fwrite(b,10)
+    q=fscanf(b,'%i')
+%     fwrite(b,20)
+%     z=fscanf(b,'%d')
+%end
 fclose(b)
 delete(b)
 clear b
+disp('Bluetooth closed succesfully')
